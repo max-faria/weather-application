5f16bbd844378271c051a36ec1e73a4f27256cd5
Max Faria <mxxfaria@gmail.com>
2023-08-05 16:08:37 -0300

    first-version

---
+++ src/api.js
@@ -0,0 +1,13 @@
+
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "561150c899mshdab2614c510918bp121f6ejsnecf964de2152",
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "d43a0ee04986d0ef2c22987293d25b3b";
